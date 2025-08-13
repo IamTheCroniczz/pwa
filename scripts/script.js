5a446fee0568e5816c2d9a837be04ea646578ed4
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2025-08-13 17:05:05 -0300

    pela milesima vez tentando resolver os bugs

--- scripts/script.js
+++ scripts/script.js
@@ -6,9 +6,3 @@ toggle.addEventListener("click", () => {
   drawer.classList.toggle("open");
 });
 
-// Registrar Service Worker
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/service-worker.js")
-    .then(() => console.log("Service Worker registrado com sucesso!"))
-    .catch(err => console.log("Erro ao registrar SW:", err));
-}
