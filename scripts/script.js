263908d863cc741258474aa6cdc486a18660da6e
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2025-08-13 17:13:21 -0300

    pela milesima vez tentando resolver os bugs

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,8 @@
-(() => {
-  // Menu toggle
-  const toggle = document.getElementById("menu-toggle");
-  const drawer = document.getElementById("drawer-menu");
+// Menu toggle
+const toggle = document.getElementById("menu-toggle");
+const drawer = document.getElementById("drawer-menu");
 
-  if (toggle && drawer) {
-    toggle.addEventListener("click", () => {
-      drawer.classList.toggle("open");
-    });
-  }
-})();
+toggle.addEventListener("click", () => {
+  drawer.classList.toggle("open");
+});
+
